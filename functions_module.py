b889ea7a75d78860fd464e3bbbe453a1666ba6e5
el-salahi <eel-salahi@hotmail.co.uk>
2024-11-22 16:14:08 +0000

    Added hardcoded weather data to main.py, imported function module. Created welcome function and user input function

---
+++ functions_module.py
@@ -0,0 +1,19 @@
+# function to display welcome message
+def welcome_message(name):
+    print(f"Welcome {name}! Your weather app will display the current weather data in your area!")
+
+# function to ask for city name
+def get_city_name():
+    city = input("Enter city to see weather results in your area: ")
+    return city
+    
+
+
+
+
+
+
+# def code_validation():
+#     try:
+#     except
+#     finally:
