953579439d3e0058c1b3acbfd2309c945618834e
el-salahi <eel-salahi@hotmail.co.uk>
2024-11-22 16:37:44 +0000

    created fetch_data.py and moved hardcoded data there, so to simulate fetching data. Imported in main.py and assigned data to variable weather_data

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
-weather_data = { "London": {"temperature": "15°C", "conditions": "Cloudy", 
-"wind_speed": "5 km/h", "humidity": "80%"}, "New York": {"temperature": 
-"20°C", "conditions": "Sunny", "wind_speed": "10 km/h", "humidity": 
-"50%"}, "Tokyo": {"temperature": "18°C", "conditions": "Rainy", 
-"wind_speed": "7 km/h", "humidity": "90%"}, "Sydney": {"temperature": 
-"22°C", "conditions": "Windy", "wind_speed": "15 km/h", "humidity": 
-"60%"}, "Paris": {"temperature": "17°C", "conditions": "Foggy", 
-"wind_speed": "3 km/h", "humidity": "85%"} }
-
+#import my modules from function_modules.py
 import functions_module
 
+#simulate fetching data from API - importing data from fetch_data.py
+import fetch_data
+
+# assigning to variable weather_data
+weather_data = fetch_data.weather_data
+
+
+# run welcome
 functions_module.welcome_message("Ella")
 
+# run get city name and assign result to a variable
 user_city = functions_module.get_city_name()
-print(user_city)
+# print(user_city)
+
