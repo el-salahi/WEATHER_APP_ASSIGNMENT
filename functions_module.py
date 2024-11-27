eafe35da385c8b235bf84a02cc5f1ddd22c41693
el-salahi <eel-salahi@hotmail.co.uk>
2024-11-27 11:02:47 +0000

    bug fix. changed data validation if to be conditional on regex. imported re and used search to find N/A data

--- functions_module.py
+++ functions_module.py
@@ -1,14 +1,15 @@
+import re
 # function to display welcome message
 def welcome_message(name):
-    print(f"Welcome {name}! Your weather app will display the current weather data in your area!")
+    print(f"Welcome {name}! Your weather app will display the current weather forecast in your area!")
 
 # function to prompt inputting city name
 def get_city_name():
-    city = input("Enter city to see weather results in your area: ")
+    city = input("Enter city to see the weather forecast in your area: ")
     return city
 
 # function to prompt input and validate value inputted
-def input_validate():
+def input_validation():
     # try block of code, else catch any potential errors with except below, then finally msg "op complete")
     try:
         # loop infinitely until condition (correct input data type) met
@@ -17,14 +18,22 @@ def input_validate():
             try:
                 # prompt user to input city via get_city_function function and store value in a variable
                 user_city = get_city_name()
-                # if the variable value is an alphabetic str then return value out of function and break loop 
-                if user_city.isalpha():
-                    print(f"{user_city} has a valid datatype")
-                    return user_city
-                    break
-                # if the variable value is anything else - e.g. int, float, !£$% non-alpha characters - then raise error, and loop begins again, prompting inputting another city
-                else:
+
+                # variable regex any characters EXECPT alphabetic (lower and upper) or white space
+                regex = r"[^a-zA-Z/\s/]"
+                # search and return match obj if any regex found in inputted user city - boolean true. If it only has alpha or \s then boolean false
+                match = bool(re.search(regex,  user_city))
+
+                # if match true - nonvalid char found
+                if match:
                     raise ValueError("Invalid datatype. Please input valid city")
+                
+                # if match false - valid char (alpha or \s)
+                else:
+                    print("Input has valid datatype")
+
+                    # if valid input data then return it to be used in global scope 
+                    return user_city
             except ValueError as ve:
                 print(f"Error due to: {ve}")
     # catch value error
@@ -38,12 +47,31 @@ def input_validate():
         print("Validation operation complete")
 
 
-# importing for the purpose of testing function
-# ! to be deleted
-import fetch_data
-weather_data = fetch_data.weather_data
-# !
 # function to display weather_data
-def display_data():
+def display_data(city):
+    for key, value in weather_data.items():
+        if key.lower() == city.lower():
+            print(f"Weather forecast incoming for {key}\n...")
+            print(f"Conditions are {value["conditions"]} in {key} today:\nIt is {value["temperature"]}, with a wind speed of {value["wind_speed"]} and humidity of {value["humidity"]}")
+            return 
+        
+    print(f"Sorry, unable to display results for {city}")
+
+
+def again_or_final(name):
+    while True:
+        yes_no = input("Would you like to see the weather forecast for a different city? Yes/No: ")
+
+        if yes_no.lower() == "yes":
+
+            valid_user_city = input_validation()
     
-    print(f"Weather conditions are {condition} today\nThe temperature is {temp}, with Wind speeds of {wind_speed} and a Humidiy of {humidity}")
+            display_data(valid_user_city.lower())
+        
+        else:
+            print(f"Thankyou for using the weather forecast app {name}. See you again next time!")
+            break
+
+
+
+
--- main.py
+++ main.py
@@ -4,17 +4,18 @@ import functions_module
 #simulate fetching data from API - importing data from fetch_data.py
 import fetch_data
 
+import re
+
 # assigning to variable weather_data
 weather_data = fetch_data.weather_data
-
 
 # run welcome
 functions_module.welcome_message("Ella")
 
-# run get city name and assign result to a variable
-# user_city = functions_module.get_city_name()
-# print(user_city)
+# get user input with a valid data type
+valid_user_city = functions_module.input_validation()
 
+# check user input is in weather_data, display data
+functions_module.display_data(valid_user_city)
 
-valid_user_city = functions_module.input_validate()
-print(valid_user_city)
+functions_module.again_or_final("Ella")
